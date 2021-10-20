99f7fa4f11e084c5a0c1d0d36c1acf91a4fcb92f
Kimberly Alves <kimberlyalves254@gmail.com>
2021-10-19 23:06:40 -0300

    Criando a classe Pokemon e os arquivos principais

---
+++ arquivo.py
@@ -0,0 +1,38 @@
+class Pokemon:
+    def __init__(self, altura, velocidade, peso, ataque, defesa, id, nome):
+        self.__altura = float(altura)
+        self.__velocidade = float(velocidade)
+        self.__peso = float(peso)
+        self.__ataque = float(ataque)
+        self.__defesa = float(defesa)
+        self.__id = int(id)
+        self.__nome = str(nome)
+
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @property
+    def velocidade(self):
+        return self.__velocidade
+
+    @property
+    def peso(self):
+        return self.__peso
+
+    @property
+    def ataque(self):
+        return self.__ataque
+
+    @property
+    def defesa(self):
+        return self.__defesa 
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def nome(self):
+        return self.__nome
